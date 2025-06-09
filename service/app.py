e7cbb9c514323cdffa2175278899ad419550b2f3
labintsev <andrej.labintsev@yandex.ru>
2025-06-09 20:13:26 +0300

    stage 8: add http-auth authorization by token

--- service/app.py
+++ service/app.py
@@ -31,6 +31,18 @@ app = Flask(__name__)
 from flask_cors import CORS
 cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
+import dotenv
+config = dotenv.dotenv_values('.env')
+
+from flask_httpauth import HTTPTokenAuth
+auth = HTTPTokenAuth(scheme='Bearer')
+tokens = { config['APP_TOKEN']: "user1", }
+
+@auth.verify_token
+def verify_token(token):
+    if token in tokens:
+        return tokens[token]
+
 import joblib
 
 # Сохранение модели
@@ -45,6 +57,7 @@ def index():
 
 # Маршрут для обработки данных формы
 @app.route("/api/numbers", methods=["POST"])
+@auth.login_required
 def process_numbers():
 
     data = request.get_json()
