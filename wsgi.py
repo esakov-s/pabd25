6c3e129b317d4e41e1fe387d4e478cdfa0f7c8e4
labintsev <andrej.labintsev@yandex.ru>
2025-06-18 19:38:24 +0300

    add model to wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import sys
 import logging
-logging.basicConfig(stream=sys.stderr)
-
+import joblib
 from service.app import app as application
+logging.basicConfig(stream=sys.stderr)
+model = 'models\decision_tree_reg_1.pkl'
+
 app = application
+app.config["model"] = joblib.load(model)
+app.logger.info(f"Use model: {model}")
