b20fd655dd114eb4add24bd322daafdd3167a7bf
labintsev <andrej.labintsev@yandex.ru>
2025-06-18 20:57:55 +0300

    fix wsgi path

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import logging
 import joblib
 from service.app import app as application
 logging.basicConfig(stream=sys.stderr)
-model = 'models\decision_tree_reg_1.pkl'
+model = 'models/decision_tree_reg_1.pkl'
 
 app = application
 app.config["model"] = joblib.load(model)
